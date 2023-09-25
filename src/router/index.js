1c2f52e3a8e9ded02b6bd2e3027bb217d9008d69
Putra Prasetya <putraprasetya333@gmail.com>
2023-09-25 22:34:19 +0700

    Menambahkan halaman profile

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import PrivateRoute from "../components/Global/PrivateRoute";
 import HomePage from "../pages/Home";
 import LoginPage from "../pages/Login";
 import RegisterPage from "../pages/Register";
+import ProfilePage from "../pages/Profile";
 import NotFoundPage from "../pages/NotFound";
 import "../app.css";
 
@@ -16,6 +17,7 @@ const Router = () => {
 					<Route path="/" element={<HomePage />} />
 					<Route path="/login" element={<LoginPage />} />
 					<Route path="/register" element={<RegisterPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
 
 					<Route path="*" element={<NotFoundPage />} />
 				</Route>
