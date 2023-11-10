ee596618a0ffe5b1b8d7b680b1be43d0e60aff87
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-11-10 14:17:57 +0700

    clean

--- src/pages/Profile/Costumer/address.js
+++ src/pages/Profile/Costumer/address.js
@@ -12,7 +12,6 @@ const Address = () => {
 	const [newAddress, setNewAddress] = useState("");
 	const [address, setAddress] = useState([]);
 	const [primaryAddress, setPrimaryAddress] = useState(false);
-	const [openModal, setOpenModal] = useState(false);
 
 	useEffect(() => {
 		if (role == 1) {
@@ -48,7 +47,6 @@ const Address = () => {
 		httpJson()
 			.post(`${baseUrl}/address`, data)
 			.then(() => {
-				setOpenModal(false);
 				Swal.fire({
 					name: "Add address success",
 					text: "Congratulations!",
@@ -60,7 +58,6 @@ const Address = () => {
 			})
 			.catch((err) => {
 				console.log(err);
-				setOpenModal(false);
 				Swal.fire({
 					name: "Add address fail",
 					text: "Add address fail",
@@ -90,8 +87,7 @@ const Address = () => {
 					<button
 						className="btn-add-address mt-4"
 						data-bs-toggle="modal"
-						data-bs-target="#newAddressModal"
-						onClick={() => setOpenModal(true)}>
+						data-bs-target="#newAddressModal">
 						<p className="fw-bold mt-3">Add new address</p>
 					</button>
 
@@ -113,136 +109,130 @@ const Address = () => {
 				</div>
 			</div>
 
-			{openModal && (
-				<div
-					class="modal"
-					id="newAddressModal"
-					tabindex="-1"
-					aria-labelledby="newAddressModalLabel">
-					<div class="modal-dialog modal-dialog-centered modal-lg">
-						<div class="modal-content">
-							<div class="modal-header border border-bottom-0">
-								<button
-									type="button"
-									class="btn-close"
-									data-bs-dismiss="modal"
-									aria-label="Close"></button>
+			<div
+				class="modal fade"
+				id="newAddressModal"
+				tabindex="-1"
+				aria-labelledby="newAddressModalLabel">
+				<div class="modal-dialog modal-dialog-centered modal-lg">
+					<div class="modal-content">
+						<div class="modal-header border border-bottom-0">
+							<button
+								type="button"
+								class="btn-close"
+								data-bs-dismiss="modal"
+								aria-label="Close"></button>
+						</div>
+						<div class="modal-body p-5 pt-1">
+							<div className="d-flex flex-column align-items-center">
+								<h3 className="fw-bold mb-4">Add new address</h3>
+								<form className="row g-3">
+									<div className="col-12">
+										<label for="address-as" className="form-label fs-input">
+											Save address as (ex : home address, office address)
+										</label>
+										<input
+											name="address_type"
+											type="text"
+											onChange={handleChange}
+											className="form-control field-inmodal"
+											id="address-as"
+											placeholder="Rumah"
+										/>
+									</div>
+
+									<div className="col-6 col-full">
+										<label for="name" className="form-label fs-input">
+											Recipient's name
+										</label>
+										<input
+											name="name"
+											type="text"
+											onChange={handleChange}
+											className="form-control field-inmodal"
+											id="name"
+										/>
+									</div>
+									<div className="col-6 col-full">
+										<label for="telp" className="form-label fs-input">
+											Recipient's telephone number
+										</label>
+										<input
+											name="phone"
+											type="number"
+											onChange={handleChange}
+											className="form-control field-inmodal"
+											id="telp"
+										/>
+									</div>
+
+									<div className="col-6 col-full">
+										<label for="addressInput" className="form-label fs-input">
+											Address
+										</label>
+										<input
+											name="street"
+											type="text"
+											onChange={handleChange}
+											className="form-control field-inmodal"
+											id="addressInput"
+										/>
+									</div>
+									<div className="col-6 col-full">
+										<label for="postal-code" className="form-label fs-input">
+											Postal code
+										</label>
+										<input
+											name="postal_code"
+											type="number"
+											onChange={handleChange}
+											className="form-control field-inmodal"
+											id="postal-code"
+										/>
+									</div>
+
+									<div className="col-6 col-full">
+										<label for="city" className="form-label fs-input">
+											City or Subdistrict
+										</label>
+										<input
+											name="city"
+											type="text"
+											onChange={handleChange}
+											className="form-control field-inmodal"
+											id="city"
+										/>
+									</div>
+
+									<div className="col-12">
+										<div className="form-check">
+											<input
+												checked={primaryAddress === true}
+												onClick={() => setPrimaryAddress(!primaryAddress)}
+												className="form-check-input p-2"
+												type="checkbox"
+												id="check-address"
+												style={{ border: "1px solid #9B9B9B" }}
+											/>
+											<label className="form-check-label fs-input" for="check-address">
+												Make it the primary address
+											</label>
+										</div>
+									</div>
+								</form>
 							</div>
-							<div class="modal-body p-5 pt-1">
-								<div className="d-flex flex-column align-items-center">
-									<h3 className="fw-bold mb-4">Add new address</h3>
-									<form className="row g-3">
-										<div className="col-12">
-											<label for="address-as" className="form-label fs-input">
-												Save address as (ex : home address, office address)
-											</label>
-											<input
-												name="address_type"
-												type="text"
-												onChange={handleChange}
-												className="form-control field-inmodal"
-												id="address-as"
-												placeholder="Rumah"
-											/>
-										</div>
-
-										<div className="col-6 col-full">
-											<label for="name" className="form-label fs-input">
-												Recipient's name
-											</label>
-											<input
-												name="name"
-												type="text"
-												onChange={handleChange}
-												className="form-control field-inmodal"
-												id="name"
-											/>
-										</div>
-										<div className="col-6 col-full">
-											<label for="telp" className="form-label fs-input">
-												Recipient's telephone number
-											</label>
-											<input
-												name="phone"
-												type="number"
-												onChange={handleChange}
-												className="form-control field-inmodal"
-												id="telp"
-											/>
-										</div>
-
-										<div className="col-6 col-full">
-											<label for="addressInput" className="form-label fs-input">
-												Address
-											</label>
-											<input
-												name="street"
-												type="text"
-												onChange={handleChange}
-												className="form-control field-inmodal"
-												id="addressInput"
-											/>
-										</div>
-										<div className="col-6 col-full">
-											<label for="postal-code" className="form-label fs-input">
-												Postal code
-											</label>
-											<input
-												name="postal_code"
-												type="number"
-												onChange={handleChange}
-												className="form-control field-inmodal"
-												id="postal-code"
-											/>
-										</div>
-
-										<div className="col-6 col-full">
-											<label for="city" className="form-label fs-input">
-												City or Subdistrict
-											</label>
-											<input
-												name="city"
-												type="text"
-												onChange={handleChange}
-												className="form-control field-inmodal"
-												id="city"
-											/>
-										</div>
-
-										<div className="col-12">
-											<div className="form-check">
-												<input
-													checked={primaryAddress === true}
-													onClick={() => setPrimaryAddress(!primaryAddress)}
-													className="form-check-input p-2"
-													type="checkbox"
-													id="check-address"
-													style={{ border: "1px solid #9B9B9B" }}
-												/>
-												<label className="form-check-label fs-input" for="check-address">
-													Make it the primary address
-												</label>
-											</div>
-										</div>
-									</form>
-								</div>
-							</div>
-							<div class="modal-footer">
-								<button
-									type="button"
-									class="btn-cancel"
-									// onClick={() => setOpenModal(false)}
-								>
-									Close
-								</button>
-								<button onClick={() => handleAddAddress()} class="btn-save">
-									Save changes
-								</button>
-							</div>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn-cancel" data-bs-dismiss="modal">
+								Close
+							</button>
+							<button onClick={() => handleAddAddress()} class="btn-save">
+								Save changes
+							</button>
 						</div>
 					</div>
 				</div>
-			)}
+			</div>
 		</div>
 	);
 };
